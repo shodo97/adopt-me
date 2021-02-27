37ad7dcbe2ab5679e2731a448179c63e739db7dc
shodo97 <sho.16@outlook.com>
2021-02-27 13:01:17 +0530

    add Details, add custom Babel config, add carousel

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,44 @@
 import React from "react";
+import Carousel from "./Carousel";
+import pet from "@frontendmasters/pet";
 
-const Details = () => {
-  return <h1>hi lol!</h1>;
-};
+class Details extends React.Component {
+  state = { loading: true };
 
+  componentDidMount() {
+    pet
+      .animal(+this.props.id)
+      .then(({ animal }) => {
+        this.setState({
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          desciption: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false,
+        });
+      })
+      .catch((err) => this.setState({ error: err }));
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>loading … </h1>;
+    }
+
+    const { animal, breed, location, desciption, media, name } = this.state;
+
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal}-${breed}-${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{desciption}</p>
+        </div>
+      </div>
+    );
+  }
+}
 export default Details;
